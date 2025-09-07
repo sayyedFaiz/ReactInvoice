102981fd5fea247ff2ff98fcb15f6f3ed81a1186
sayyedFaiz <sayyedfaiz7@gmail.com>
2025-09-07 19:39:05 +0200

    deleted postcss file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: ['9wyrca-ip-46-223-148-61.tunnelmole.net'],
-    host: true,
-    port: 5173,
-  }
-})
+  plugins: [ tailwindcss()],
+});

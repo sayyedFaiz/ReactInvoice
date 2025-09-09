c53c7b4dbe8fba3a4470a29ef10a599f6bcda191
sayyedFaiz <sayyedfaiz7@gmail.com>
2025-09-09 19:20:06 +0200

    cors issue

--- server/index.js
+++ server/index.js
@@ -7,14 +7,9 @@ import customerRoutes from "./routes/customerRoutes.js"
 dotenv.config();
 const app = express();
 app.use(express.json())
-// Example for Express.js
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-;
+app.use(cors({
+    origin: '*'
+}));
 connectDB()
 
 const PORT = process.env.PORT || 5000;
